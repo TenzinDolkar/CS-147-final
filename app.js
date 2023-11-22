2c61829d93dc266ce0da64d06e2ffea4e03afdf7
Tenzin Dolkar <tdolkar2016@DNa809b6c.SUNet>
2023-11-22 15:25:18 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+import { Link, Stack } from "expo-router/";
+import { useState, useEffect } from "react";
+import {
+  Button,
+  StyleSheet,
+  SafeAreaView,
+  Text,
+  Image,
+  Pressable,
+  FlatList,
+  View,
+} from "react-native";
+import { Themes, Images } from "../assets/Themes";
+import * as React from "react";
+
+<View > 
+  <Text style={styles.text}> Kith&Kin </Text>
+</View>
+
+
+const styles = StyleSheet.create({
+  text: {
+    font: "Corben"
+    color: "#387F58"
+  }
+});
