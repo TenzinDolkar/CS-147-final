cda092b63949325ded136d3081294e0bcd64bfa6
Haven Kei <havenw@stanford.edu>
2023-12-06 11:33:50 -0800

    Finished profile page and added under construction alert

--- app/components/profile.js
+++ app/components/profile.js
@@ -1,4 +1,5 @@
 import { StyleSheet, Text, View, Image, Dimensions } from "react-native";
+import { useState } from "react";
 
 import { Link } from "expo-router";
 
@@ -15,6 +16,19 @@ import {
   ButtonIcon,
 } from '@gluestack-ui/themed';
 
+import {
+  AlertDialog,
+  AlertDialogBackdrop,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogCloseButton,
+  AlertDialogFooter,
+  AlertDialogBody,
+  Heading,
+  CloseIcon,
+  ButtonGroup,
+} from "@gluestack-ui/themed";
+
 import Icons from "../../assets/Icons";
 
 import { config } from "@gluestack-ui/config";
@@ -29,13 +43,14 @@ const GREEN = "#387F58";
 const FUSCHIA = "#E45B74";
 
 
-export default function Profile() {
+export default function Profile({ navigation}) {
+  const [showConstruction, setShowConstruction] = useState(false);
   return (
     <GluestackUIProvider config={config}>
     <View style={styles.container}>
       <View style={styles.main}>
         
-      <Avatar bgColor="$rose200" size="2xl">
+      <Avatar bgColor="$rose200" size="2xl" style={styles.profilePic}>
         {/* User is imported from 'lucide-react-native' */}
         {/* <Icon as={Icons.profileFilled.nav} color="white" size="lg" /> */}
         <Image source={require("../../assets/Icons/Profile_Icon.png")} />
@@ -52,13 +67,71 @@ export default function Profile() {
             size="md"
             borderRadius="$none"
             isDisabled={false}
-            onPress={() => setShowAlertDialog(true)}
+            onPress={() => setShowConstruction(true)}
             style={styles.button}
           >
-            <ButtonText color={GREEN}>RSVP</ButtonText>
+            <ButtonText color={GREEN}>Edit</ButtonText>
+          </Button>
+          <Button
+            action="secondary"
+            size="md"
+            borderRadius="$none"
+            isDisabled={false}
+            onPress={() => setShowConstruction(true)}
+            style={styles.button}
+          >
+            <ButtonText color={GREEN}>Settings</ButtonText>
+          </Button>
+          <Button
+            action="secondary"
+            size="md"
+            borderRadius="$none"
+            isDisabled={false}
+            onPress={() => setShowConstruction(true)}
+            style={styles.button}
+          >
+            <ButtonText color={GREEN}>. . .</ButtonText>
           </Button>
         </View>
+        <Button
+            action="secondary"
+            size="md"
+            borderRadius="$none"
+            isDisabled={false}
+            onPress={() => navigation.navigate("Calendar")}
+            style={styles.button}
+            id="calendarButton"
+          >
+            <ButtonText color={GREEN}>See my calendar</ButtonText>
+          </Button>
       </View>
+
+      <AlertDialog
+        isOpen={showConstruction}
+        onClose={() => {
+          setShowConstruction(false);
+        }}
+      >
+        <AlertDialogBackdrop />
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <Heading size="lg">Under Construction</Heading>
+            <AlertDialogCloseButton>
+              <Icon as={CloseIcon} />
+            </AlertDialogCloseButton>
+          </AlertDialogHeader>
+          <AlertDialogBody>
+            <Text size="sm">This page is still under construction.</Text>
+          </AlertDialogBody>
+          <AlertDialogFooter>
+            
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+
+
+
+
     </View>
     </GluestackUIProvider>
   );
@@ -68,20 +141,73 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
+    // alignContent: "center",
     padding: 24,
+    backgroundColor: "white"
   },
   main: {
     flex: 1,
     justifyContent: "center",
-    maxWidth: 960,
-    marginHorizontal: "auto",
+    // maxWidth: 960,
+    width: "90%",
+    alignItems: "center",
+  },
+  profilePic: {
+    padding: 10,
+    margin: 10,
+    alignSelf: "center",
+    
+
   },
   title: {
-    fontSize: 64,
+    fontSize: 20,
     fontWeight: "bold",
+    textAlign: "center",
+    // borderColor: "black",
+    // borderWidth: 1,
+    color: FUSCHIA,
+    fontFamily: "Inter-Medium",
   },
   subtitle: {
     fontSize: 36,
     color: "#38434D",
+  },
+  text: {
+    margin: 10,
+  },
+  profileContainer: {
+    width: "100%",
+    // alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
+    backgroundColor: FIGMA_WHITE,
+    borderRadius: 10,
+    // borderColor: "black",
+    // borderWidth: 1,
+    margin: 10,
+  },
+  button: {
+    // width: "10%",
+    // height: "80%",
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: FIGMA_WHITE,
+    margin: 10,
+    shadowColor: "grey",
+    shadowOpacity: 0.2,
+    shadowRadius: 2,
+    shadowOffset: {
+      height: 2,
+      width: 2,
+    }
+  },
+  buttonBar: {
+    // width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    margin: 10,
+    marginTop: 20,
   },
 });
